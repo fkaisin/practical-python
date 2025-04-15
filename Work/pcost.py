a73bee1addd0e188af41b43b9aaa44a9cbe725fe
fkaisin <floriankaisin@hotmail.com>
2025-04-15 12:46:32 +0200

    $2.2 complete

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,12 +2,12 @@
 #
 # Exercise 1.27
 
-def portfolio_cost(filename = 'Data/portfolio.csv'):
-    import csv
-    import sys
+import csv
+import sys
 
+def portfolio_cost(filename = 'Data/portfolio.csv'):
     try:
-        total_cost = 0
+        total_cost = 0.0
 
         if len(sys.argv) == 2:
             filename = sys.argv[1]
@@ -18,12 +18,9 @@ def portfolio_cost(filename = 'Data/portfolio.csv'):
             for row in rows:
                 total_cost += float(row[1]) * float(row[2])
 
-        total_cost = round(total_cost,2)
-        print(f'Total cost {total_cost}')
+        # total_cost = round(total_cost,2)
+        return total_cost
+
     except FileNotFoundError:
         print('File not found.')
-
-
-# file = 'C:/Users/kaisin_f/OneDrive - sabca.be/code/practical-python/Work/Data/portfolio.csv'
-# file = 'missing.csv'
-portfolio_cost()
+        return 0.0
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,63 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+from pprint import pprint
+
+def read_portfolio(file_name):
+
+    portfolio = []
+
+    try:
+        with open(file_name, 'rt') as f:
+            rows = csv.reader(f)
+            headers = next(rows)
+            for row in rows:
+                try:
+                    holding = {'name':row[0], 'shares':int(row[1]), 'price':float(row[2])}
+                    portfolio.append(holding)
+                except IndexError:
+                    pass
+            
+    except FileNotFoundError:
+        print('File not found.')
+        portfolio = None
+
+    return portfolio
+
+def read_prices(file_name):
+
+    prices = {}
+
+    try:
+        with open(file_name, 'rt') as f:
+            rows = csv.reader(f)
+            for row in rows:
+                try:
+                    prices[row[0]] = float(row[1])
+                except IndexError:
+                    pass
+
+    except FileNotFoundError:
+        print('File not found.')
+        prices = None
+
+    return prices
+
+# # Calcul du gain total
+
+# file = 'Work/Data/portfolio.csv'
+# portfolio = read_portfolio(file)
+
+# file = 'Work/Data/prices.csv'
+# prices = read_prices(file)
+
+# total_gains = 0.0
+
+# for s in portfolio:
+#     gain = s['shares'] * (prices[s['name']] - s['price'])
+#     total_gains += gain
+#     print(f'Gains sur {s['name']} : {round(gain,2)}')
+
+# print('Gain total : ', total_gains)
